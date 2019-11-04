71692ccc6b2c853576d4c3fa33ea506f716a3929
dracangelo <kelvinmbuguaw@gmail.com>
2019-11-04 08:39:17 +0300

    added a hover to first row

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,43 @@
 $(document).ready(function() {
   $("#pp").click(function() {
-    $("#dd").toggle(slow);
+    $("#dd").toggle(3000);
   });
   $("#tt").click(function() {
-    $("#uu").toggle(slow);
+    $("#uu").toggle(3000);
   });
   $("#gg").click(function() {
-    $("#mm").toggle(slow);
+    $("#mm").toggle(3000);
   });
+  $("#pic").hover(
+    function() {
+      $(".text1").show("slow");
+    },
+    function() {
+      $(".text1").hide("slow");
+    }
+  );
+  $("#nop").hover(
+      function() {
+          $(".text2").show("slow");
+      },
+      function() {
+          $(".text2").hide("slow");
+      },
+  );
+  $("#noo").hover (
+      function() {
+          $(".text3").show("slow");
+      },
+      function() {
+          $(".text3").hide("slow")
+      }
+  );
+  $("#yep").hover (
+      function() {
+          $(".text4").show("slow");
+      },
+      function() {
+          $(".text4").hide("slow")
+      }
+  );
 });
-
