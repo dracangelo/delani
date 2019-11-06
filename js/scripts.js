6b3aa82cbb7b0c6fafccf20b8634cd000a99de63
dracangelo <kelvinmbuguaw@gmail.com>
2019-11-06 08:27:41 +0300

    toggled what about us

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,28 @@
 $(document).ready(function() {
+  
+  $("#gg").click(function() {
+    $("#mm").show("1000");
+    $("#gg").hide("1000");
+  });
+  $("#mm").click(function() {
+    $("#gg").show("1000");
+    $("#mm").hide("1000");
+  });
   $("#pp").click(function() {
-    $("#dd").toggle(1000);
+    $("#dd").show("1000");
+    $("#pp").hide("1000");
+  });
+  $("#dd").click(function() {
+    $("#pp").show("1000");
+    $("#dd").hide("1000");
   });
   $("#tt").click(function() {
-    $("#uu").toggle(1000);
+    $("#uu").show("1000");
+    $("#tt").hide("1000");
   });
-  $("#gg").click(function() {
-    $("#mm").toggle(1000);
+  $("#uu").click(function() {
+    $("#tt").show("1000");
+    $("#uu").hide("1000");
   });
   $("#pic").hover(
     function() {
@@ -73,14 +89,16 @@ $(document).ready(function() {
     }
   );
 });
-function submit() {
+
+function click(event) {
   var name = document.getElementById("name");
   var email = document.getElementById("email");
   var message = document.getElementById("meso");
-  if (email === "") {
+  if (email == "") {
     alert("please enter email");
   } else {
     alert("Thank you for reaching out to us!");
   }
+  alert("Niaje");
   event.preventDefault();
 }
